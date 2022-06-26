9d02e0f20739e02d523d21e2e9d4637e9728970e
Chaoneng Tan <chaonengtan@gmail.com>
2022-06-25 18:51:01 -0700

    orb vallis

--- components/ov.js
+++ components/ov.js
@@ -1,8 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { useQuery } from '@apollo/client'
+import GetWorldState from '../functions/WorldStateQuery'
+import parseTime from '../functions/parseTime'
+
+// image
+import ov from './static/images/OV.jpeg'
 export default function OV() {
+    const { data } = useQuery(
+        GetWorldState,
+        { 
+          variables: { location: 'vallisCycle'},
+          pollInterval: 1000
+        }
+    )
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Orb Vallis</Text>
-        </View>
+        <ImageBackground source={ov}>
+            <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                {data ? <>
+                    <Text style={styles.text}>Orb Vallis | {data.getWorldstate.state}</Text>
+                    <Text style={styles.text}>{parseTime(data.getWorldstate.timeLeft)}</Text>
+                </> : <Text style={styles.text}>Loading</Text>}
+            </View>
+        </ImageBackground>
     )
 }
+const styles = StyleSheet.create({
+    text: { color: 'white' }
+})
