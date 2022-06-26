0a65414947be12710fc14a57fa056cc758557b68
Chaoneng Tan <chaonengtan@gmail.com>
2022-06-25 18:50:56 -0700

    cambion drift

--- components/cd.js
+++ components/cd.js
@@ -1,8 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { useQuery } from '@apollo/client'
+import GetWorldState from '../functions/WorldStateQuery'
+import parseTime from '../functions/parseTime'
+
+// image
+import cd from './static/images/CD.jpeg'
 export default function CD() {
+    const { data } = useQuery(
+        GetWorldState,
+        { 
+          variables: { location: 'cambionCycle'},
+          pollInterval: 1000
+        }
+    )
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Cambion Drift</Text>
-        </View>
+        <ImageBackground source={cd}>
+            <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                {data ? <>
+                    <Text style={styles.text}>Cambion Drift | {data.getWorldstate.state}</Text>
+                    <Text style={styles.text}>{parseTime(data.getWorldstate.timeLeft)}</Text>
+                </> : <Text style={styles.text}>Loading</Text>}
+            </View>
+        </ImageBackground>
     )
 }
+const styles = StyleSheet.create({
+    text: { color: 'white' }
+})
